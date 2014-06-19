5f3698d37dbfebdd430c6755eef508cacde0f6a3
dennis84 <d.dietrich84@gmail.com>
2014-06-19 07:48:07 +0200

    cursor with tooltip

--- src/collab/editor.cljs
+++ src/collab/editor.cljs
@@ -66,10 +66,17 @@
   (str (- (:x c) 1) "ch"))
 
 (q/defcomponent Cursor [c]
-  (d/div {:className "cursor-container"}
-    (d/div {:className "cursor"
+  (q/on-mount
+    (d/div {:className "cursor-container"
             :style {:top (get-cursor-top c) :left (get-cursor-left c)}
-            } (:member c))))
+            :title (:member c)
+            :data-placement "top"}
+      (d/div {:className "cursor"}))
+    (fn [node]
+      (let [$elem (js/$ node)]
+        (.tooltip $elem (clj->js {:container $elem}))
+        (.tooltip $elem "show")
+        (js/setTimeout #(.tooltip $elem "hide") 3000)))))
 
 (defn- scroll-to [node pos]
   (let [$elem (js/$ node)
